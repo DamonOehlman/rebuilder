b3cb6106bb24dfec7e03f5e71c9bd899a4582091
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-23 20:57:23 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var path = require('path');
+var chokidar = require('chokidar');
+var parallel = require('run-parallel');
+
+module.exports = function(server, opts) {
+  var cwd = (opts || {}).cwd || path.dirname(module.parent.filename);
+  var srcPath = (opts || {}).src || path.resolve(cwd, 'src');
+  var staticPath = (opts || {}).static || path.resolve(cwd, 'static');
+
+  // initialise the actions
+  var actions = ((opts || {}).actions || []).concat([
+    [ /\.js$/i, require('./lib/browserify') ],
+    [ /\.css$/i, require('./lib/cssnext') ]
+  ]);
+
+  var watcher = chokidar.watch(srcPath);
+  var buildTasks = [];
+
+  function createBuildTasks(filename) {
+    return actions.map(function(action) {
+      var match = action[0].exec(filename);
+      if (match) {
+        return action[1].bind(null, filename, 'foo', opts);
+      }
+    }).filter(Boolean);
+  }
+
+  function rebuild() {
+    parallel(buildTasks, function(err, results) {
+      if (err) {
+        watcher.close();
+        return console.error(err);
+      }
+
+      console.log('run rebuild');
+    });
+  }
+
+  fs.readdir(srcPath, function(err, files) {
+    buildTasks = (files || []).reduce(function(items, filename) {
+      return items.concat(createBuildTasks(filename));
+    }, []);
+
+    // build the buildable files, and then start the server
+    rebuild();
+  });
+
+  watcher.on('all', function(evt, filename) {
+    console.log('file changed: ', evt, path.relative(srcPath, filename));
+  });
+};
